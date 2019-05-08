1e683c79f6841c3561b731bfff3469b8c32293c9
Lachezar Lechev <lachezar@adex.network>
2019-05-08 19:12:00 +0300

    Cargo.toml - use feature derive for serde

--- sentry/src/domain/channel.rs
+++ sentry/src/domain/channel.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
-use serde_derive::*;
+use serde::{Serialize, Deserialize};
 
 //use crate::domain::BigNum;
 use crate::domain::ValidatorDesc;
--- sentry/src/domain/validator.rs
+++ sentry/src/domain/validator.rs
@@ -1,4 +1,4 @@
-use serde_derive::*;
+use serde::{Serialize, Deserialize};
 
 use crate::domain::BigNum;
 
