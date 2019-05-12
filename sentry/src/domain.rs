809176d27143d8501aa7f3c242f95c2419e67c0e
Lachezar Lechev <lachezar@adex.network>
2019-05-12 12:42:28 +0300

    Domain error: - add DomainError struct - BigNum - `new()` fn with Domain error by default

--- sentry/src/domain.rs
+++ sentry/src/domain.rs
@@ -1,3 +1,6 @@
+use std::error;
+use std::fmt;
+
 pub use bignum::BigNum;
 pub use channel::{Channel, ChannelSpec};
 pub use validator::ValidatorDesc;
@@ -5,3 +8,18 @@ pub use validator::ValidatorDesc;
 mod bignum;
 mod channel;
 mod validator;
+
+#[derive(Debug, )]
+pub struct DomainError;
+
+impl fmt::Display for DomainError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Domain error",)
+    }
+}
+
+impl error::Error for DomainError {
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+}
