cedab076ac8a9c2c11e6f2de27119e76224ca576
Lachezar Lechev <lachezar@adex.network>
2019-05-12 12:40:59 +0300

    BigNum: - application - resource - channel_create - use BigNum - domain - channel - use BigNum

--- sentry/src/application/resource/channel/channel_create/input.rs
+++ sentry/src/application/resource/channel/channel_create/input.rs
@@ -2,12 +2,14 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use tower_web::Extract;
 
+use crate::domain::BigNum;
+
 #[derive(Extract, Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ChannelInput {
     pub id: String,
     pub creator: String,
     pub deposit_asset: String,
-    pub deposit_amount: i32,
+    pub deposit_amount: BigNum,
     pub valid_until: DateTime<Utc>,
 }
