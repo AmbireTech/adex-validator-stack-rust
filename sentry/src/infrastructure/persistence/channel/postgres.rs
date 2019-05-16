c94c29511963780b8d0472eeddff378fb750a896
Lachezar Lechev <lachezar@adex.network>
2019-05-16 13:13:15 +0300

    PostgresChannelRepository - implement ChannelRepository

--- sentry/src/infrastructure/persistence/channel/postgres.rs
+++ sentry/src/infrastructure/persistence/channel/postgres.rs
@@ -1,11 +1,11 @@
 use futures::compat::Future01CompatExt;
+use futures::future::FutureExt;
 use futures_legacy::Future as OldFuture;
 use futures_legacy::future::IntoFuture;
 use futures_legacy::Stream as OldStream;
 use try_future::try_future;
 
-use crate::application::error::ApplicationError;
-use crate::domain::{Channel, RepositoryError};
+use crate::domain::{Channel, ChannelRepository, RepositoryError, RepositoryFuture};
 use crate::infrastructure::persistence::DbPool;
 
 pub struct PostgresChannelRepository {
@@ -16,8 +16,10 @@ impl PostgresChannelRepository {
     pub fn new(db_pool: DbPool) -> Self {
         Self { db_pool }
     }
+}
 
-    pub async fn list(&self) -> Result<Vec<Channel>, RepositoryError> {
+impl ChannelRepository for PostgresChannelRepository {
+    fn list(&self) -> RepositoryFuture<Vec<Channel>> {
         let fut = self.db_pool
             .run(move |mut conn| {
                 conn.prepare("SELECT channel_id, creator, deposit_asset, deposit_amount, valid_until FROM channels")
@@ -52,11 +54,11 @@ impl PostgresChannelRepository {
             })
             .map_err(|err| handle_internal_error(&err));
 
-        await!(fut.compat()).map_err(|err| handle_internal_error(&err))
+        fut.compat().boxed()
     }
 
-    pub async fn insert(&self, channel: Channel) -> Result<Channel, ApplicationError> {
-        Ok(channel)
+    fn create(&self, channel: Channel) -> RepositoryFuture<()> {
+        futures::future::ok(()).boxed()
     }
 }
 
