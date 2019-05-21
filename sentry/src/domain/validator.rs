5181aaea52a78105d9cab36ef8d64d43b85873bd
Lachezar Lechev <lachezar@adex.network>
2019-05-21 17:21:22 +0300

    domain - validator - fixtures - add `get_validator` & `get_validators` fns

--- sentry/src/domain/validator.rs
+++ sentry/src/domain/validator.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 use crate::domain::BigNum;
 
@@ -8,4 +8,38 @@ pub struct ValidatorDesc {
     id: String,
     url: String,
     fee: BigNum,
+}
+
+#[cfg(test)]
+pub(crate) mod fixtures {
+    use std::convert::TryFrom;
+
+    use chrono::{DateTime, Utc};
+    use fake::faker::*;
+
+    use crate::domain::BigNum;
+
+    use super::ValidatorDesc;
+
+    pub fn get_validator(validator_id: &str) -> ValidatorDesc {
+        let fee = BigNum::try_from(<Faker as Number>::between(1_u32, 13_u32)).expect("BigNum error when creating from random number");
+        let url = format!("http://{}-validator-url.com/validator", validator_id);
+
+        ValidatorDesc {
+            id: validator_id.to_string(),
+            url,
+            fee,
+        }
+    }
+
+    pub fn get_validators(count: usize, prefix: Option<&str>) -> Vec<ValidatorDesc> {
+        let prefix = prefix.map_or(String::new(), |prefix| format!("{}-", prefix));
+        (0..count)
+            .map(|c| {
+                let validator_id = format!("{}validator-{}", prefix, c + 1);
+
+                get_validator(&validator_id)
+            })
+            .collect()
+    }
 }
