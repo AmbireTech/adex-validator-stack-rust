a8f8ee1ad8da46babd332b691c52b8b57674a059
Lachezar Lechev <lachezar@adex.network>
2019-05-21 13:25:22 +0300

    `page` & `validator` query parameters to Channel List resource. - resource - channel_list - handler - add `limit_per_page` to the struct - resource - channel_list - handler - pass the `page` to `handle()` - resource - channel - create a ChannelListQuery - resource - channel - use ChannelListQuery and pass things to handler

--- sentry/src/application/resource/channel/channel_list/handler.rs
+++ sentry/src/application/resource/channel/channel_list/handler.rs
@@ -6,19 +6,20 @@ use super::ChannelListResponse;
 use chrono::Utc;
 
 pub struct ChannelListHandler<'a> {
+    limit_per_page: u32,
     channel_repository: &'a dyn ChannelRepository,
 }
 
 impl<'a> ChannelListHandler<'a> {
-    pub fn new(channel_repository: &'a ChannelRepository) -> Self {
-        Self { channel_repository }
+    pub fn new(limit_per_page: u32, channel_repository: &'a ChannelRepository) -> Self {
+        Self { limit_per_page, channel_repository }
     }
 }
 
 impl<'a> ChannelListHandler<'a> {
-    pub async fn handle(&self) -> Result<ChannelListResponse, ()> {
+    pub async fn handle(&self, page: u32) -> Result<ChannelListResponse, ()> {
         // @TODO: pass the correct page from Query and use the application limit per page
-        let list_fut = self.channel_repository.list(Utc::now(), 1, 200);
+        let list_fut = self.channel_repository.list(Utc::now(), page, self.limit_per_page);
         // @TODO: Proper error handling
         let channels = await!(list_fut).unwrap();
 
