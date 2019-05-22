a13d25e8d8a5ddc3db4394d6bc3bde82b6648f87
Lachezar Lechev <lachezar@adex.network>
2019-05-22 13:44:53 +0300

    infra - persistence - add a new enum defining whether we use Postgres or Memory for persistence

--- sentry/src/infrastructure/persistence.rs
+++ sentry/src/infrastructure/persistence.rs
@@ -3,3 +3,8 @@ pub mod memory;
 pub mod postgres;
 
 pub type DbPool = bb8::Pool<bb8_postgres::PostgresConnectionManager<tokio_postgres::NoTls>>;
+
+pub enum Persistence {
+    Memory,
+    Postgres(DbPool),
+}
