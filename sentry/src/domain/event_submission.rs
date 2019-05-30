69e517efb2b3570a5f2f37f5d3117f0e703a29e7
Lachezar Lechev <lachezar@adex.network>
2019-05-30 12:43:35 +0300

    domain - EventSubmission & Rule + RateLimit

--- sentry/src/domain/event_submission.rs
+++ sentry/src/domain/event_submission.rs
@@ -1,4 +1,26 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct EventSubmission {}
+pub struct EventSubmission {
+    #[serde(default)]
+    allow: Vec<Rule>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Rule {
+    #[serde(default)]
+    uids: Option<Vec<String>>,
+    #[serde(default)]
+    rate_limit: Opiton<RateLimit>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RateLimit {
+    /// "ip", "uid"
+    limit_type: String,
+    /// in milliseconds
+    #[serde(remame = "timeframe")]
+    time_frame: u64,
+}
