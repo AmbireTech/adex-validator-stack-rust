57acb8a50ce883a0fd1b992d19b8226b3c9cfbd8
Lachezar Lechev <lachezar@adex.network>
2019-05-31 11:20:08 +0300

    sentry - init-db - channel_id should be 66 chars "0x" prefix + 2(hex) * 32 chars

--- sentry/init-db.sql
+++ sentry/init-db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE channels
 (
-    channel_id     VARCHAR(255)             NOT NULL,
+    channel_id     VARCHAR(66)              NOT NULL,
     creator        VARCHAR(255)             NOT NULL,
     deposit_asset  VARCHAR(42)              NOT NULL,
     deposit_amount VARCHAR(255)             NOT NULL, -- @TODO change the deposit to BigNum compatible field
