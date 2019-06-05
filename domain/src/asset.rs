747edfdab71402a87a2084bc18d6fe3220b226c2
Lachezar Lechev <lachezar@adex.network>
2019-06-05 14:14:33 +0300

    domain - Asset - derive `PartialEq` & `Eq`

--- domain/src/asset.rs
+++ domain/src/asset.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Asset(String);
 
 impl From<String> for Asset {
