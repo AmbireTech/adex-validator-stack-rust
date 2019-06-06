733a345b3b5d80a0bcd481b8b14b6660e41858b3
Lachezar Lechev <lachezar@adex.network>
2019-06-06 11:58:49 +0300

    [+test] adapter - dummy - `sign` & `verify` docs-tests + a unit test

--- adapter/src/dummy.rs
+++ adapter/src/dummy.rs
@@ -13,6 +13,18 @@ impl Adapter for DummyAdapter {
         &self.config
     }
 
+    /// Example:
+    ///
+    /// ```
+    /// use adapter::{ConfigBuilder, Adapter};
+    /// use adapter::dummy::DummyAdapter;
+    ///
+    /// let config = ConfigBuilder::new("identity").build();
+    /// let adapter = DummyAdapter { config };
+    ///
+    /// let actual = adapter.sign("abcdefghijklmnopqrstuvwxyz012345");
+    /// assert_eq!("Dummy adapter signature for 6162636465666768696a6b6c6d6e6f707172737475767778797a303132333435 by identity", &actual);
+    /// ```
     fn sign(&self, state_root: &str) -> String {
         format!(
             "Dummy adapter signature for {} by {}",
@@ -21,14 +33,42 @@ impl Adapter for DummyAdapter {
         )
     }
 
-    /// Sample signature
-    /// `Dummy adapter for 6def5a300acb6fcaa0dab3a41e9d6457b5147a641e641380f8cc4bf5308b16fe by awesomeLeader`
+    /// Example:
+    ///
+    /// ```
+    /// use adapter::{ConfigBuilder, Adapter};
+    /// use adapter::dummy::DummyAdapter;
+    ///
+    /// let config = ConfigBuilder::new("identity").build();
+    /// let adapter = DummyAdapter { config };
+    ///
+    /// assert!(adapter.verify("identity", "doesn't matter", "Dummy adapter signature for 6162636465666768696a6b6c6d6e6f707172737475767778797a303132333435 by identity") )
+    /// ```
     fn verify(&self, signer: &str, _state_root: &str, signature: &str) -> bool {
-        // select the `awesomeLeader` and compare it to the signer
+        // select the `identity` and compare it to the signer
         // for empty string this will return array with 1 element - an empty string `[""]`
         match signature.rsplit(' ').take(1).next() {
             Some(from) => from == signer,
             None => false,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::adapter::ConfigBuilder;
+
+    #[test]
+    fn sings_state_root_and_verifies_it() {
+        let config = ConfigBuilder::new("identity").build();
+        let adapter = DummyAdapter { config };
+
+        let expected_signature = "Dummy adapter signature for 6162636465666768696a6b6c6d6e6f707172737475767778797a303132333435 by identity";
+        let actual_signature = adapter.sign("abcdefghijklmnopqrstuvwxyz012345");
+
+        assert_eq!(expected_signature, &actual_signature);
+
+        assert!(adapter.verify("identity", "doesn't matter", &actual_signature))
     }
 }
