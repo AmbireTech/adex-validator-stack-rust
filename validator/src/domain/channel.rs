48369ba404bc16d6ae413534459d8d3816c72a99
Lachezar Lechev <lachezar@adex.network>
2019-06-11 09:21:07 +0300

    validator - domain - fix `ChannelRepository::all`

--- validator/src/domain/channel.rs
+++ validator/src/domain/channel.rs
@@ -1,6 +1,6 @@
 use domain::{BigNum, Channel, ChannelId, RepositoryFuture};
 
 pub trait ChannelRepository: Send + Sync {
-    /// Returns list of all channels, based on the passed Parameters for this method
-    fn all(&self, channel_id: &ChannelId) -> RepositoryFuture<Vec<Channel>>;
+    /// Returns list of all channels, based on the passed validator identity
+    fn all(&self, identity: &str) -> RepositoryFuture<Vec<Channel>>;
 }
