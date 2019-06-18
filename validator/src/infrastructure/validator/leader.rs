4ebec9637b564c221000378d86ae9605b5914a01
Lachezar Lechev <lachezar@adex.network>
2019-06-18 18:02:33 +0300

    validator - domain - Validator - `tick()` to method and add `Channel`

--- validator/src/infrastructure/validator/leader.rs
+++ validator/src/infrastructure/validator/leader.rs
@@ -1,10 +1,13 @@
+use domain::Channel;
+
 use crate::domain::validator::{Validator, ValidatorFuture};
+use futures::future::FutureExt;
 
 #[derive(Clone)]
 pub struct Leader {}
 
 impl Validator for Leader {
-    fn tick() -> ValidatorFuture<()> {
-        unimplemented!()
+    fn tick(&self, _channel: Channel) -> ValidatorFuture<()> {
+        futures::future::ok(()).boxed()
     }
 }
