9fd996aea29f2517717f57a7846a83a61a7ab3d1
Lachezar Lechev <lachezar@adex.network>
2019-06-18 13:18:11 +0300

    validator - persistence - channel - api - use `SentryApi`

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1,16 +1,18 @@
 #![feature(async_await, await_macro)]
 #![deny(rust_2018_idioms)]
 #![deny(clippy::all)]
-use reqwest::r#async::Client;
 
 use futures::future::{FutureExt, TryFutureExt};
+use reqwest::r#async::Client;
+
 use validator::domain::channel::ChannelRepository;
+use validator::infrastructure::persistence::channel::api::ApiChannelRepository;
+use validator::infrastructure::sentry::SentryApi;
 
 fn main() {
     let future = async {
-        let repo = validator::infrastructure::persistence::channel::api::ApiChannelRepository {
-            client: Client::new(),
-        };
+        let sentry = SentryApi { client: Client::new(), sentry_url: "http://localhost:8005".into() };
+        let repo = ApiChannelRepository { sentry };
 
         let all_channels = await!(repo.all("0x2892f6C41E0718eeeDd49D98D648C789668cA67d"));
 
