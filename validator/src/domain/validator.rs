8abe36457f4f68d08cfb4a5df1ec1c1c61e5c593
Lachezar Lechev <lachezar@adex.network>
2019-06-21 13:33:29 +0300

    validator - domain - validator - MessageRepository

--- validator/src/domain/validator.rs
+++ validator/src/domain/validator.rs
@@ -4,6 +4,8 @@ use futures::Future;
 
 use domain::Channel;
 
+pub use self::repository::MessageRepository;
+
 pub type ValidatorFuture<T> = Pin<Box<dyn Future<Output = Result<T, ValidatorError>> + Send>>;
 
 #[derive(Debug)]
@@ -13,4 +15,14 @@ pub enum ValidatorError {
 
 pub trait Validator {
     fn tick(&self, channel: Channel) -> ValidatorFuture<()>;
+}
+
+pub mod repository {
+    use domain::validator::message::State;
+    use domain::validator::Message;
+    use domain::RepositoryFuture;
+
+    pub trait MessageRepository {
+        fn add<S: State>(message: Message<S>) -> RepositoryFuture<()>;
+    }
 }
