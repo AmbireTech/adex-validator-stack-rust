c6c147880ef29ea840bdf4bd9a5b9048e1a5226a
Lachezar Lechev <lachezar@adex.network>
2019-06-21 13:34:04 +0300

    validator - persistence - validator - memory - dummy MemoryMessageRepository

---
+++ validator/src/infrastructure/persistence/validator/memory.rs
@@ -0,0 +1,13 @@
+use domain::validator::message::State;
+use domain::validator::Message;
+use domain::RepositoryFuture;
+
+use crate::domain::validator::repository::MessageRepository;
+
+pub struct MemoryValidatorRepository {}
+
+impl MessageRepository for MemoryValidatorRepository {
+    fn add<S: State>(_message: Message<S>) -> RepositoryFuture<()> {
+        unimplemented!()
+    }
+}
