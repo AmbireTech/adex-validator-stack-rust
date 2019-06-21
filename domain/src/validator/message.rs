1e4ddd2b8a063d7888586c25bae4c080b14a3ee4
Lachezar Lechev <lachezar@adex.network>
2019-06-21 10:04:10 +0300

    domain - validator - message - fixtures - add simple fixtures

--- domain/src/validator/message.rs
+++ domain/src/validator/message.rs
@@ -1,9 +1,9 @@
 use chrono::{DateTime, Utc};
+use serde::de::DeserializeOwned;
+use serde::export::fmt::Debug;
 use serde::{Deserialize, Serialize};
 
 use crate::BalancesMap;
-use serde::de::DeserializeOwned;
-use serde::export::fmt::Debug;
 
 pub trait State {
     type Signature: DeserializeOwned + Serialize + Debug;
@@ -59,31 +59,64 @@ pub struct Accounting {
 
 #[cfg(any(test, feature = "fixtures"))]
 pub mod fixtures {
+    use fake::faker::*;
+
     use crate::test_util::time::past_datetime;
 
     use super::*;
 
-    pub fn get_approve_state<S: State>() -> ApproveState<S> {
-        unimplemented!()
+    pub fn get_approve_state<S: State>(
+        state_root: S::StateRoot,
+        signature: S::Signature,
+        is_healthy: bool,
+    ) -> ApproveState<S> {
+        ApproveState {
+            state_root,
+            signature,
+            is_healthy,
+        }
     }
 
-    pub fn get_new_state<S: State>() -> NewState<S> {
-        unimplemented!()
+    pub fn get_new_state<S: State>(
+        state_root: S::StateRoot,
+        signature: S::Signature,
+        balances: BalancesMap,
+    ) -> NewState<S> {
+        NewState {
+            state_root,
+            signature,
+            balances,
+        }
     }
 
-    pub fn get_reject_state() -> RejectState {
-        unimplemented!()
+    pub fn get_reject_state(reason: Option<String>) -> RejectState {
+        RejectState {
+            reason: reason.unwrap_or_else(|| <Faker as Lorem>::sentence(5, 4)),
+        }
     }
 
-    pub fn get_heartbeat<S: State>() -> Heartbeat<S> {
-        unimplemented!()
+    pub fn get_heartbeat<S: State>(signature: S::Signature) -> Heartbeat<S> {
+        Heartbeat {
+            signature,
+            timestamp: past_datetime(None),
+        }
     }
 
-    pub fn get_accounting() -> Accounting {
+    pub fn get_accounting(
+        balances: BalancesMap,
+        pre_fees: Option<BalancesMap>,
+        last_ev_aggr: Option<DateTime<Utc>>,
+    ) -> Accounting {
+        let last_event_aggregate = last_ev_aggr.unwrap_or_else(|| past_datetime(None));
+        assert!(
+            last_event_aggregate < Utc::now(),
+            "You cannot have a last_event_aggregate < Now"
+        );
+
         Accounting {
-            last_event_aggregate: past_datetime(None),
-            pre_fees: Default::default(),
-            balances: Default::default(),
+            last_event_aggregate,
+            pre_fees: pre_fees.unwrap_or_else(|| BalancesMap::default()),
+            balances,
         }
     }
 }
