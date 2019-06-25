c1ffa361ace71ad4f3bb09b01dc33444905e052f
Lachezar Lechev <lachezar@adex.network>
2019-06-25 13:15:13 +0300

    [+test] validator - application - heartbeat - HeartbeatFactory

--- validator/src/application/heartbeat.rs
+++ validator/src/application/heartbeat.rs
@@ -25,14 +25,45 @@ impl fmt::Display for HeartbeatFactoryError {
 
 impl<A: Adapter + State> HeartbeatFactory<A> {
     #[allow(clippy::needless_lifetimes)]
-    pub async fn create<S>(&self, state_root: S) -> Result<Heartbeat<A>, HeartbeatFactoryError>
-    where
-        S: Into<A::StateRoot>,
-    {
-        let state_root = state_root.into();
+    pub async fn create(
+        &self,
+        state_root: A::StateRoot,
+    ) -> Result<Heartbeat<A>, HeartbeatFactoryError> {
         let signature =
             await!(self.adapter.sign(&state_root)).map_err(HeartbeatFactoryError::Adapter)?;
 
         Ok(Heartbeat::new(signature, state_root))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use adapter::dummy::DummyAdapter;
+    use adapter::ConfigBuilder;
+
+    use super::*;
+    use chrono::Utc;
+
+    #[test]
+    fn creates_heartbeat() {
+        futures::executor::block_on(async {
+            let adapter = DummyAdapter {
+                config: ConfigBuilder::new("identity").build(),
+                participants: HashMap::default(),
+            };
+
+            let factory = HeartbeatFactory { adapter };
+
+            let state_root = "my dummy StateRoot".to_string();
+
+            let adapter_signature = await!(factory.adapter.sign(&state_root)).expect("Adapter should sign the StateRoot");
+            let heartbeat =
+                await!(factory.create(state_root)).expect("Heartbeat should be created");
+
+            assert!(Utc::now() >= heartbeat.timestamp);
+            assert_eq!(adapter_signature, heartbeat.signature);
+        });
     }
 }
