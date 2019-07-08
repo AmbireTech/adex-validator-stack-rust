f577689edb9aa0ea98ced699d0eeaefb31323fd3
Lachezar Lechev <lachezar@adex.network>
2019-07-08 14:00:59 +0300

    [+test] validator - persistence - channel - memory - add tests & fix typos

--- validator/src/infrastructure/persistence/channel/memory.rs
+++ validator/src/infrastructure/persistence/channel/memory.rs
@@ -13,11 +13,11 @@ pub struct MemoryChannelRepository {
 }
 
 impl MemoryChannelRepository {
-    pub fn new(initial_messages: &[Channel]) -> Self {
+    pub fn new(initial_channels: &[Channel]) -> Self {
         let cmp = Arc::new(|channel: &Channel, channel_id: &ChannelId| &channel.id == channel_id);
 
         Self {
-            inner: MemoryRepository::new(&initial_messages, cmp),
+            inner: MemoryRepository::new(&initial_channels, cmp),
         }
     }
 }
@@ -32,5 +32,51 @@ impl ChannelRepository for MemoryChannelRepository {
             });
 
         ready(list.map_err(Into::into)).boxed()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use domain::fixtures::{get_channel, get_channel_id, get_channel_spec, get_validator};
+    use domain::SpecValidators;
+
+    use super::*;
+
+    #[test]
+    fn find_all_channels_with_the_passed_identity_and_skips_the_rest() {
+        futures::executor::block_on(async {
+            let identity = "Lookup identity";
+            let validator_1 = get_validator(identity, None);
+            let validator_2 = get_validator("Second", None);
+            let channel_1_spec = get_channel_spec(
+                "doesn't matter",
+                Some(SpecValidators::new(
+                    validator_1.clone(),
+                    validator_2.clone(),
+                )),
+            );
+            let channel_3_spec = get_channel_spec(
+                "doesn't matter",
+                // switch leader & follower
+                Some(SpecValidators::new(
+                    validator_2.clone(),
+                    validator_1.clone(),
+                )),
+            );
+            let channels = vec![
+                get_channel("channel 1", &None, Some(channel_1_spec)),
+                get_channel("channel 2", &None, None),
+                get_channel("channel 3", &None, Some(channel_3_spec)),
+            ];
+
+            let repository = MemoryChannelRepository::new(&channels);
+
+            let result: Vec<Channel> =
+                await!(repository.all(&identity)).expect("Getting all channels failed");
+
+            assert_eq!(2, result.len());
+            assert_eq!(&get_channel_id("channel 1"), &result[0].id);
+            assert_eq!(&get_channel_id("channel 3"), &result[1].id);
+        })
     }
 }
