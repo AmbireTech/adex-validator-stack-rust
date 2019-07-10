90bd5a7af816d1dd3e3fa1c45209bb3c2395ccec
Lachezar Lechev <lachezar@adex.network>
2019-07-10 14:09:47 +0300

    validator - domain - re-export `MerkleTree`

--- validator/src/domain.rs
+++ validator/src/domain.rs
@@ -2,6 +2,8 @@ pub use self::channel::ChannelRepository;
 pub use self::validator::MessageRepository;
 pub use self::validator::{Validator, ValidatorError, ValidatorFuture};
 pub use self::worker::{Worker, WorkerFuture};
+// re-export the merkle_tree as a Validator domain struct
+pub use merkle_tree::MerkleTree;
 
 pub mod channel;
 pub mod validator;
