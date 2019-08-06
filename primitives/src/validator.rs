7fffdfd4566d9320fe9146235e0176c92bd9c54b
Omidiora Samuel <>
2019-08-06 16:10:09 +0100

    add: sentry crate

--- primitives/src/validator.rs
+++ primitives/src/validator.rs
@@ -9,6 +9,48 @@ use crate::{BigNum, DomainError};
 
 pub mod message;
 
+use std::pin::Pin;
+
+use futures::Future;
+
+use domain::Channel;
+
+pub use self::repository::MessageRepository;
+
+pub type ValidatorFuture<T> = Pin<Box<dyn Future<Output = Result<T, ValidatorError>> + Send>>;
+
+#[derive(Debug)]
+pub enum ValidatorError {
+    None,
+}
+
+pub trait Validator {
+    fn tick(&self, channel: Channel) -> ValidatorFuture<()>;
+}
+
+pub mod repository {
+    use domain::validator::message::{MessageType, State};
+    use domain::validator::{Message, ValidatorId};
+    use domain::{ChannelId, RepositoryFuture};
+
+    pub trait MessageRepository<S: State> {
+        fn add(
+            &self,
+            channel: &ChannelId,
+            validator: &ValidatorId,
+            message: Message<S>,
+        ) -> RepositoryFuture<()>;
+
+        fn latest(
+            &self,
+            channel: &ChannelId,
+            from: &ValidatorId,
+            types: Option<&[&MessageType]>,
+        ) -> RepositoryFuture<Option<Message<S>>>;
+    }
+}
+
+
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 #[serde(transparent)]
 pub struct ValidatorId(String);
