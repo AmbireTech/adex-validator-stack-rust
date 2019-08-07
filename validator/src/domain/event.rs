c1450cbe6625f9e11f748c0eb686357e2b250ac2
Lachezar Lechev <lachezar@adex.network>
2019-08-07 14:46:57 +0300

    validator - domain - event - `ad_unit` to the appropriate types and remove `ImpressionPricePerCase`

--- validator/src/domain/event.rs
+++ validator/src/domain/event.rs
@@ -7,8 +7,10 @@ use domain::{AdUnit, BigNum};
 #[serde(tag = "type", rename_all = "SCREAMING_SNAKE_CASE")]
 #[derive(Serialize, Deserialize)]
 pub enum Event {
+    #[serde(rename_all = "camelCase")]
     Impression {
         publisher: String,
+        ad_unit: AdUnit,
     },
     #[serde(rename_all = "camelCase")]
     UpdateImpressionPrice {
@@ -16,12 +18,15 @@ pub enum Event {
         price: u64,
         ad_unit: AdUnit,
     },
+    #[serde(rename_all = "camelCase")]
     ImpressionWithCommission {
         earner: Vec<Earner>,
+        ad_unit: AdUnit,
     },
-    ImpressionPricePerCase {},
+    #[serde(rename_all = "camelCase")]
     Pay {
         outputs: HashMap<String, BigNum>,
+        ad_unit: AdUnit,
     },
     PauseChannel {},
     #[serde(skip_deserializing)]
