cdbfe61749c4f45414d6d409c74c0af13f66dbd9
Omidiora Samuel <>
2019-08-08 15:47:47 +0100

    remove old folder

--- .old/d_sentry/src/application/resource/channel/channel_create.rs
+++
@@ -1,7 +0,0 @@
-pub(crate) use self::handler::ChannelCreateHandler;
-pub(crate) use self::input::ChannelInput;
-pub(crate) use self::response::ChannelCreateResponse;
-
-mod handler;
-mod input;
-mod response;
--- .old/validator/src/domain/worker.rs
+++
@@ -1,8 +0,0 @@
-use futures::Future;
-use std::pin::Pin;
-
-pub type WorkerFuture = Pin<Box<dyn Future<Output = Result<(), ()>> + Send>>;
-
-pub trait Worker {
-    fn run(&self) -> WorkerFuture;
-}
