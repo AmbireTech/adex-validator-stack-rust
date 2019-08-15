8d724cf0d5397d3ff7c53f281ebc8ebb7fea6a67
Lachezar Lechev <8925621+elpiel@users.noreply.github.com>
2019-08-15 09:43:44 +0300

    Revert "Issue #102 mergeAggrs and mergePayoutsIntoBalances (#110)"
    This reverts commit 27f50c8fa174fc4f99d50d19c95a039f36278689.

--- validator/src/domain.rs
+++ validator/src/domain.rs
@@ -4,6 +4,5 @@ pub use self::validator::{Validator, ValidatorError, ValidatorFuture};
 pub use self::worker::{Worker, WorkerFuture};
 
 pub mod channel;
-pub mod event;
 pub mod validator;
 pub mod worker;
