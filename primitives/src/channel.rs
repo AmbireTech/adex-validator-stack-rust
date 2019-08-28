749804ca3c99e5350d3f7bc3188e2d9a7e227f1f
Lachezar Lechev <lachezar@adex.network>
2019-08-28 13:21:22 +0300

    clean up

--- primitives/src/channel.rs
+++ primitives/src/channel.rs
@@ -1,13 +1,13 @@
+use std::error::Error;
 use std::fmt;
-//
+
 use chrono::serde::{ts_milliseconds, ts_seconds};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-//
+
 use crate::big_num::BigNum;
 use crate::util::serde::ts_milliseconds_option;
 use crate::{AdUnit, EventSubmission, TargetingTag, ValidatorDesc};
-use std::error::Error;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -21,7 +21,6 @@ pub struct Channel {
     pub spec: ChannelSpec,
 }
 
-//
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct ChannelSpec {
