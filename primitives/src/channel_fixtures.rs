36a65133df776d16a703a8d0ecb0abe00a995843
Lachezar Lechev <lachezar@adex.network>
2019-08-29 13:35:26 +0300

    dummy channel

--- primitives/src/channel_fixtures.rs
+++ primitives/src/channel_fixtures.rs
@@ -1,10 +1,10 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 use fake::faker::*;
 use time::Duration;
 
 use crate::targeting_tag::fixtures::get_targeting_tags;
 use crate::validator::fixtures::get_validator;
-use crate::BigNum;
+use crate::{BigNum, EventSubmission};
 
 use super::{Channel, ChannelSpec, SpecValidators, ValidatorDesc};
 use crate::util::tests::take_one;
@@ -26,6 +26,44 @@ const ASSETS_LIST: [&str; 8] = ["DAI", "BGN", "EUR", "USD", "ADX", "BTC", "LIT",
 //
 //    ChannelId { bytes: id }
 //}
+
+pub fn get_dummy_channel() -> Channel {
+    let leader = ValidatorDesc {
+        id: "awesomeLeader".to_string(),
+        url: "http://localhost:8005".to_string(),
+        fee: 100.into(),
+    };
+    let follower = ValidatorDesc {
+        id: "awesomeFollower".to_string(),
+        url: "http://localhost:8006".to_string(),
+        fee: 100.into(),
+    };
+    let nonce = BigNum::from(<Faker as Number>::between(100_000_000, 999_999_999));
+
+    Channel {
+        id: "awesomeTestChannel".to_string(),
+        creator: "awesomeCreator".to_string(),
+        deposit_asset: "DAI".to_string(),
+        deposit_amount: 1_000.into(),
+        // UNIX timestamp for 2100-01-01
+        valid_until: Utc.timestamp(4_102_444_800, 0),
+        spec: ChannelSpec {
+            title: None,
+            validators: SpecValidators([leader, follower]),
+            max_per_impression: 10.into(),
+            min_per_impression: 10.into(),
+            targeting: vec![],
+            min_targeting_score: None,
+            event_submission: EventSubmission { allow: vec![] },
+            // July 29, 2019 7:00:00 AM
+            created: Utc.timestamp(1_564_383_600, 0),
+            active_from: None,
+            nonce,
+            withdraw_period_start: Utc.timestamp_millis(4_073_414_400_000),
+            ad_units: vec![],
+        },
+    }
+}
 
 pub fn get_channel(
     id: &str,
@@ -80,8 +118,6 @@ pub enum ValidatorsOption<'a> {
 }
 
 pub fn get_channel_spec(validators_option: ValidatorsOption<'_>) -> ChannelSpec {
-    use crate::EventSubmission;
-
     let validators = match validators_option {
         ValidatorsOption::Pair { leader, follower } => [leader, follower].into(),
         ValidatorsOption::SpecValidators(spec_validators) => spec_validators,
