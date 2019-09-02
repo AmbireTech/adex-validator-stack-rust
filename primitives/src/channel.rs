5c444ff83a2ebc19062e82ba9eff6a2217ee0558
Lachezar Lechev <lachezar@adex.network>
2019-09-02 09:23:35 +0300

    channel - remove fixtures & tests as we don't need them

--- primitives/src/channel.rs
+++ primitives/src/channel.rs
@@ -8,8 +8,6 @@ use serde::{Deserialize, Serialize};
 use crate::big_num::BigNum;
 use crate::util::serde::ts_milliseconds_option;
 use crate::{AdUnit, EventSubmission, TargetingTag, ValidatorDesc};
-#[path = "./channel_fixtures.rs"]
-pub mod fixtures;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
