b1bc7c6cf786f6ccbed85836fc59667cb9fd911b
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-09-02 19:02:18 +0100

    add: merkleitem

--- primitives/src/merkle_tree.rs
+++ primitives/src/merkle_tree.rs
@@ -8,6 +8,7 @@ use merkletree::merkle;
 use merkletree::merkle::{VecStore};
 use merkletree::proof::Proof;
 use tiny_keccak::Keccak;
+use hex::ToHex;
 
 #[derive(Clone)]
 struct KeccakAlgorithm(Keccak);
@@ -36,9 +37,11 @@ impl Hasher for KeccakAlgorithm {
     }
 }
 
-impl Algorithm<[u8; 32]> for KeccakAlgorithm {
+pub type MerkleItem = [u8; 32];
+
+impl Algorithm<MerkleItem> for KeccakAlgorithm {
     #[inline]
-    fn hash(&mut self) -> [u8; 32] {
+    fn hash(&mut self) -> MerkleItem {
         let mut res: [u8; 32] = [0; 32];
         self.0.clone().finalize(&mut res);
         res
@@ -50,27 +53,47 @@ impl Algorithm<[u8; 32]> for KeccakAlgorithm {
     }
 }
 
-pub struct MerkleTree(merkle::MerkleTree<[u8; 32], KeccakAlgorithm, VecStore<[u8; 32]>>);
+pub struct MerkleTree(merkle::MerkleTree<MerkleItem, KeccakAlgorithm, VecStore<MerkleItem>>);
 
 impl MerkleTree {
-    pub fn new(leaves: Vec<[u8; 32]>) -> MerkleTree {
+    pub fn new(leaves: Vec<MerkleItem>) -> MerkleTree {
         let t = merkle::MerkleTree::from_iter(leaves);
         Self(t)
     }
 
-    pub fn root(&self)-> [u8; 32] {
+    pub fn root(&self)-> MerkleItem {
         self.0.root()
     }
 
-    pub fn verify(&self, proof: (Vec<[u8; 32]>, Vec<bool>)) -> bool {
+    pub fn verify(&self, proof: (Vec<MerkleItem>, Vec<bool>) ) -> bool {
         let proof = Proof::new(proof.0, proof.1);
         proof.validate::<KeccakAlgorithm>()
     }
 
-    pub fn proof(&self, i: usize) -> (Vec<bool>, Vec<[u8; 32]>) {
+    pub fn proof(&self, i: usize) -> (Vec<MerkleItem>, Vec<bool>) {
         let proof = self.0.gen_proof(i);
         let path = proof.path();
         let lemma = proof.lemma();
-        (path.to_owned(), lemma.to_owned())
+        (lemma.to_owned(), path.to_owned())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_gets_the_root_of_single_item() {
+        let mut h1 = [0u8; 32];
+        let mut h2 = [0u8; 32];
+        let mut h3 = [0u8; 32];
+        h1[0] = 0x11;
+        h2[0] = 0x22;
+        h3[0] = 0x33;
+
+        let t: MerkleTree = MerkleTree::new(vec![h1, h2, h3]);
+        let proof =  t.proof(1);
+        let verify = t.verify(proof.clone());
+        assert_eq!(verify, true, "should verify proof successfully");
     }
 }
