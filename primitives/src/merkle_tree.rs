95d227f019f5579402c99b4408791c7c07121c35
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-09-03 06:48:11 +0100

    fix: remove changes

--- primitives/src/merkle_tree.rs
+++ primitives/src/merkle_tree.rs
@@ -47,16 +47,32 @@ impl Algorithm<MerkleItem> for KeccakAlgorithm {
         res
     }
 
-    #[inline]
-    fn reset(&mut self) {
-        self.0 = Keccak::new_sha3_256();
+    fn leaf(&mut self, leaf: MerkleItem) -> MerkleItem {
+        self.write(leaf.as_ref());
+        self.hash()
+    }
+
+    fn node(&mut self, left: MerkleItem, right: MerkleItem, _height: usize) -> MerkleItem {
+        let mut buf: Vec<u8> = left.iter().cloned().collect();
+        let mut buf1: Vec<u8> = right.iter().cloned().collect();
+        buf.append(&mut buf1);
+        buf.sort();
+        println!(" buf len {}", buf.len());
+
+        let mut buf_slice: [u8; 64] = [0; 64];
+        buf_slice.copy_from_slice(buf.as_slice());
+
+        self.write(buf_slice.as_ref());
+        self.hash()
     }
 }
 
 pub struct MerkleTree(merkle::MerkleTree<MerkleItem, KeccakAlgorithm, VecStore<MerkleItem>>);
 
 impl MerkleTree {
-    pub fn new(leaves: Vec<MerkleItem>) -> MerkleTree {
+    pub fn new(_leaves: Vec<MerkleItem>) -> MerkleTree {
+        let mut leaves = _leaves;
+        leaves.sort();
         let t = merkle::MerkleTree::from_iter(leaves);
         Self(t)
     }
@@ -83,7 +99,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn it_gets_the_root_of_single_item() {
+    fn it_generates_correct_proof() {
         let mut h1 = [0u8; 32];
         let mut h2 = [0u8; 32];
         let mut h3 = [0u8; 32];
@@ -92,6 +108,30 @@ mod test {
         h3[0] = 0x33;
 
         let t: MerkleTree = MerkleTree::new(vec![h1, h2, h3]);
+        let proof =  t.proof(1);
+        let verify = t.verify(proof.clone());
+        assert_eq!(verify, true, "should verify proof successfully");
+    }
+
+    #[test]
+    fn it_works_okay_with_js_impl() {
+        let mut h1 = hex::decode("item").unwrap();
+        let mut h2 = hex::decode("item").unwrap();
+        println!("len {}", h1.len());
+        let mut h1_slice: [u8; 32] = Default::default();
+        h1_slice.copy_from_slice(h1.as_slice());
+
+        println!("{:?}", hex::encode(h1_slice));
+
+        let mut h2_slice: [u8; 32] = Default::default();
+        h2_slice.copy_from_slice(h2.as_slice());
+
+        println!("{:?}", hex::encode(h1_slice));
+
+        let t: MerkleTree = MerkleTree::new(vec![h1_slice, h2_slice]);
+        let root = t.root();
+        println!("root {:?}", hex::encode(root));
+        // let leaves = t.
         let proof =  t.proof(1);
         let verify = t.verify(proof.clone());
         assert_eq!(verify, true, "should verify proof successfully");
