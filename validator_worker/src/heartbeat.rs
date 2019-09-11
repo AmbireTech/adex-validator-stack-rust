676c19565e683e0e3be367a43235844a807b631d
Lachezar Lechev <lachezar@adex.network>
2019-09-11 11:21:41 +0300

    heartbeat - wrap the current work for heartbeat and leave some todos for the rest of the tasks

--- validator_worker/src/heartbeat.rs
+++ validator_worker/src/heartbeat.rs
@@ -1,36 +1,59 @@
 use std::error::Error;
 
-use primitives::{BalancesMap, Channel, BigNum};
 use primitives::adapter::Adapter;
+use primitives::{BalancesMap, BigNum, Channel};
 
 use crate::sentry_interface::SentryApi;
-use primitives::validator::MessageTypes;
-use chrono::Utc;
 use chrono::Duration;
+use chrono::Utc;
+use primitives::validator::{Heartbeat, MessageTypes};
 
-async fn send_heartbeat<A: Adapter + 'static>(_iface: &SentryApi<A>) -> () {
+async fn send_heartbeat<A: Adapter + 'static>(iface: &SentryApi<A>) -> Result<(), Box<dyn Error>> {
+    let timestamp = Utc::now();
+    // TODO: create the MerkleTree
 
+    // TODO: get the root
+
+    // TODO: create stateRootRaw
+    let state_root_raw = "state_root_raw".to_owned();
+    // TODO: get the Hex of State root
+    let state_root = "0x.....".to_string();
+    let signature = iface.adapter.sign(&state_root_raw)?;
+
+    iface.propagate(&[MessageTypes::Heartbeat(Heartbeat {
+        signature,
+        state_root,
+        timestamp,
+    })]);
+
+    Ok(())
 }
 
-pub async fn heartbeat<A: Adapter + 'static>(iface: &SentryApi<A>, balances: BalancesMap, heartbeat_time: u32) -> Result<(), Box<dyn Error>> {
+pub async fn heartbeat<A: Adapter + 'static>(
+    iface: &SentryApi<A>,
+    balances: BalancesMap,
+    heartbeat_time: u32,
+) -> Result<(), Box<dyn Error>> {
     let validator_message_response = await!(iface.get_our_latest_msg("Heartbeat".into()))?;
 
-    let heartbeat_msg = validator_message_response.msg.get(0).and_then(|message_types| {
-        match message_types {
+    let heartbeat_msg = validator_message_response
+        .msg
+        .get(0)
+        .and_then(|message_types| match message_types {
             MessageTypes::Heartbeat(heartbeat) => Some(heartbeat.clone()),
             _ => None,
-        }
-    });
+        });
     let should_send = match heartbeat_msg {
         Some(heartbeat) => {
             let duration = Utc::now() - heartbeat.timestamp;
-            duration > Duration::milliseconds(heartbeat_time.into()) && is_channel_not_exhausted(&iface.channel, &balances)
-        },
+            duration > Duration::milliseconds(heartbeat_time.into())
+                && is_channel_not_exhausted(&iface.channel, &balances)
+        }
         None => true,
     };
 
     if should_send {
-        await!(send_heartbeat(&iface));
+        await!(send_heartbeat(&iface))?;
     }
 
     Ok(())
