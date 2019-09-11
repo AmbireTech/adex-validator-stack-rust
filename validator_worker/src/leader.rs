7d049786a631205abae81fd710e76d1358277b83
Lachezar Lechev <lachezar@adex.network>
2019-09-11 14:15:03 +0300

    leader::tick() + get_state_root_hash()

--- validator_worker/src/leader.rs
+++ validator_worker/src/leader.rs
@@ -1,12 +1,37 @@
+use std::error::Error;
+
 use futures::future::FutureExt;
-use primitives::validator::{Validator, ValidatorFuture};
-use primitives::Channel;
 
-#[derive(Clone)]
-pub struct Leader {}
+use primitives::adapter::Adapter;
+use primitives::{Channel, BalancesMap};
+use primitives::validator::{Validator, ValidatorFuture, Accounting, MessageTypes, NewState};
+use crate::{producer, get_state_root_hash};
 
-impl Validator for Leader {
-    fn tick(&self, _channel: Channel) -> ValidatorFuture<()> {
-        futures::future::ok(()).boxed()
+use crate::sentry_interface::SentryApi;
+
+pub async fn tick<A: Adapter + 'static>(iface: &SentryApi<A>) -> Result<(), Box<dyn Error>> {
+    let (balances, new_accounting) = await!(producer::tick(&iface))?;
+
+    if let Some(new_accounting) = new_accounting {
+        await!(on_new_accounting(&iface, (&balances, &new_accounting)))?;
     }
+
+    Ok(())
+}
+
+fn on_new_accounting<A: Adapter + 'static>(iface: &SentryApi<A>, (balances, new_accounting): (&BalancesMap, &Accounting)) -> Result<(), Box<dyn Error>> {
+    let state_root_raw = get_state_root_hash(&iface, &balances);
+
+    let signature = iface.adapter.sign(&state_root_raw)?;
+
+    // TODO: Get Hex state root
+    let state_root = state_root_raw.clone();
+
+    iface.propagate(&[&MessageTypes::NewState(NewState {
+        state_root,
+        signature,
+        balances: new_accounting.balances.clone()
+    })])?;
+
+    Ok(())
 }
--- validator_worker/src/lib.rs
+++ validator_worker/src/lib.rs
@@ -11,11 +11,20 @@ pub mod producer;
 pub mod sentry_interface;
 
 pub use self::follower::Follower;
-pub use self::leader::Leader;
 pub use self::sentry_interface::all_channels;
+use crate::sentry_interface::SentryApi;
+use primitives::adapter::Adapter;
+use primitives::BalancesMap;
 
 pub mod core {
     pub mod events;
     pub mod fees;
     pub mod follower_rules;
+}
+
+pub(crate) fn get_state_root_hash<A: Adapter + 'static>(
+    _iface: &SentryApi<A>,
+    _balances: &BalancesMap,
+) -> String {
+    unimplemented!("Still need implementation")
 }
