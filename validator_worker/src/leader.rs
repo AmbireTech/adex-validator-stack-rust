0b9e5e3d8dce962a7377c0156efc21db7e25d995
Lachezar Lechev <lachezar@adex.network>
2019-09-16 11:51:11 +0300

    leader - finish up the `on_new_accounting`

--- validator_worker/src/leader.rs
+++ validator_worker/src/leader.rs
@@ -1,37 +1,36 @@
 use std::error::Error;
 
-use futures::future::FutureExt;
-
+use primitives::BalancesMap;
 use primitives::adapter::Adapter;
-use primitives::{Channel, BalancesMap};
-use primitives::validator::{Validator, ValidatorFuture, Accounting, MessageTypes, NewState};
-use crate::{producer, get_state_root_hash};
+use primitives::validator::{Accounting, MessageTypes, NewState};
 
+use crate::{get_state_root_hash, producer};
 use crate::sentry_interface::SentryApi;
 
 pub async fn tick<A: Adapter + 'static>(iface: &SentryApi<A>) -> Result<(), Box<dyn Error>> {
     let (balances, new_accounting) = await!(producer::tick(&iface))?;
 
     if let Some(new_accounting) = new_accounting {
-        await!(on_new_accounting(&iface, (&balances, &new_accounting)))?;
+        on_new_accounting(&iface, (&balances, &new_accounting))?;
     }
 
     Ok(())
 }
 
-fn on_new_accounting<A: Adapter + 'static>(iface: &SentryApi<A>, (balances, new_accounting): (&BalancesMap, &Accounting)) -> Result<(), Box<dyn Error>> {
-    let state_root_raw = get_state_root_hash(&iface, &balances);
+fn on_new_accounting<A: Adapter + 'static>(
+    iface: &SentryApi<A>,
+    (balances, new_accounting): (&BalancesMap, &Accounting),
+) -> Result<(), Box<dyn Error>> {
+    let state_root_raw = get_state_root_hash(&iface, &balances)?;
+    let state_root = hex::encode(state_root_raw);
 
-    let signature = iface.adapter.sign(&state_root_raw)?;
-
-    // TODO: Get Hex state root
-    let state_root = state_root_raw.clone();
+    let signature = iface.adapter.sign(&hex::encode(&state_root))?;
 
     iface.propagate(&[&MessageTypes::NewState(NewState {
         state_root,
         signature,
-        balances: new_accounting.balances.clone()
-    })])?;
+        balances: new_accounting.balances.clone(),
+    })]);
 
     Ok(())
 }
