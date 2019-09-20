ee95a1edffc12a38640131b6d27104a7d42cefd5
Lachezar Lechev <lachezar@adex.network>
2019-09-20 12:11:19 +0300

    validator_worker leader - fix double `hex::encode`ing `state_root` before `adapter.sign()`

--- validator_worker/src/leader.rs
+++ validator_worker/src/leader.rs
@@ -25,7 +25,7 @@ fn on_new_accounting<A: Adapter + 'static>(
     let state_root_raw = get_state_root_hash(&iface, &balances)?;
     let state_root = hex::encode(state_root_raw);
 
-    let signature = iface.adapter.sign(&hex::encode(&state_root))?;
+    let signature = iface.adapter.sign(&state_root)?;
 
     iface.propagate(&[&MessageTypes::NewState(NewState {
         state_root,
