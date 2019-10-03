5fcdc94bb8dc595ebedcf4220bf3994f7fba3cf1
Lachezar Lechev <lachezar@adex.network>
2019-10-03 14:46:34 +0300

    sentry - add the cli options, load config and make a 404 Not found response.

--- sentry/src/main.rs
+++ sentry/src/main.rs
@@ -1,106 +1,133 @@
 #![deny(clippy::all)]
 #![deny(rust_2018_idioms)]
-use std::convert::TryFrom;
-use std::env::Vars;
-use std::net::SocketAddr;
 
-use futures::compat::Future01CompatExt;
-use futures::future::{FutureExt, TryFutureExt};
-use tokio::await;
-use tokio_tcp::TcpListener;
-use tower_web::ServiceBuilder;
+use clap::{App, Arg};
+use hyper::{Body, Error, Request, Response, Server, StatusCode};
+use hyper::service::{make_service_fn, service_fn};
 
-use domain::DomainError;
-use lazy_static::lazy_static;
-use sentry::application::resource::channel::ChannelResource;
-use sentry::infrastructure::persistence::channel::{
-    MemoryChannelRepository, PostgresChannelRepository,
-};
-use sentry::infrastructure::persistence::DbPool;
-use std::sync::Arc;
+use adapter::{AdapterTypes, DummyAdapter, EthereumAdapter};
+use primitives::adapter::{Adapter, AdapterOptions};
+use primitives::config::configuration;
+use primitives::util::tests::prep_db::{AUTH, IDS};
 
 const DEFAULT_PORT: u16 = 8005;
 
-lazy_static! {
-    static ref CONFIG: Config = {
-        dotenv::dotenv().ok();
-        Config::try_from(std::env::vars()).expect("Config failed")
+#[tokio::main]
+async fn main() {
+    let cli = App::new("Sentry")
+        .version("0.1")
+        .arg(
+            Arg::with_name("config")
+                .help("the config file for the validator worker")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("adapter")
+                .short("a")
+                .help("the adapter for authentication and signing")
+                .required(true)
+                .default_value("ethereum")
+                .possible_values(&["ethereum", "dummy"])
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("keystoreFile")
+                .short("k")
+                .help("path to the JSON Ethereum Keystore file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("dummyIdentity")
+                .short("i")
+                .help("the identity to use with the dummy adapter")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("clustered")
+                .short("c")
+                .help("Run app in cluster mode with multiple workers"),
+        )
+        .get_matches();
+
+    let environment = std::env::var("ENV").unwrap_or("development".into());
+    let port = std::env::var("PORT")
+        .map(|s| s.parse::<u16>().expect("Invalid port(u16) was provided"))
+        .unwrap_or_else(|_| DEFAULT_PORT);
+    let config_file = cli.value_of("config");
+    let config = configuration(&environment, config_file).unwrap();
+    let clustered = cli.is_present("clustered");
+
+    let _adapter = match cli.value_of("adapter").unwrap() {
+        "ethereum" => {
+            let keystore_file = cli
+                .value_of("keystoreFile")
+                .expect("keystore file is required for the ethereum adapter");
+            let keystore_pwd = std::env::var("KEYSTORE_PWD").unwrap();
+
+            let options = AdapterOptions {
+                keystore_file: Some(keystore_file.to_string()),
+                keystore_pwd: Some(keystore_pwd),
+                dummy_identity: None,
+                dummy_auth: None,
+                dummy_auth_tokens: None,
+            };
+            let ethereum_adapter = EthereumAdapter::init(options, &config).expect("Should initialize ethereum adapter");
+
+            AdapterTypes::EthereumAdapter(Box::new(ethereum_adapter))
+        }
+        "dummy" => {
+            let dummy_identity = cli
+                .value_of("dummyIdentity")
+                .expect("Dummy identity is required for the dummy adapter");
+
+            let options = AdapterOptions {
+                dummy_identity: Some(dummy_identity.to_string()),
+                // @TODO: this should be prefilled using fixtures
+                dummy_auth: Some(IDS.clone()),
+                dummy_auth_tokens: Some(AUTH.clone()),
+                keystore_file: None,
+                keystore_pwd: None,
+            };
+
+            let dummy_adapter = DummyAdapter::init(options, &config).expect("Should initialize dummy adapter");
+            AdapterTypes::DummyAdapter(Box::new(dummy_adapter))
+        }
+        // @TODO exit gracefully
+        _ => panic!("We don't have any other adapters implemented yet!"),
     };
+
+    run(clustered, port).await;
 }
 
-pub fn main() {
-    // @TODO: Define and use a CLI for setting sentry options
+//async fn run(config: Config, adapter: AdapterTypes, _clustered: bool, port: u16) {
+async fn run(_clustered: bool, port: u16) {
+    let addr = ([127, 0, 0, 1], port).into();
 
-    let port: u16 = std::env::var("PORT")
-        .unwrap_or_else(|_| format!("{}", DEFAULT_PORT))
-        .parse()
-        .expect("Failed to parse port");
-    let database_url = std::env::var("DATABASE_URL").expect("Missing DATABASE_URL");
+    let make_service = make_service_fn(|_| async {
+        Ok::<_, Error>(service_fn(|req| async {
+            Ok::<_, Error>(handle_routing(req))
+        }))
+    });
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let server = Server::bind(&addr).serve(make_service);
 
-    println!("Listening on http://{}", addr);
-
-    tokio::run(bootstrap(database_url, addr).unit_error().boxed().compat())
-}
-
-async fn bootstrap(database_url: String, addr: SocketAddr) {
-    // @TODO: Error handling
-    let db_pool = await!(database_pool(database_url)).expect("Database connection failed");
-
-    let listener = TcpListener::bind(&addr).expect("Wrong address provided");
-
-    let channel_repository = Arc::new(MemoryChannelRepository::new(None));
-    let _channel_repository = Arc::new(PostgresChannelRepository::new(db_pool.clone()));
-
-    // A service builder is used to configure our service.
-    let server = ServiceBuilder::new()
-        .resource(ChannelResource {
-            channel_list_limit: CONFIG.channel_list_limit,
-            channel_repository: channel_repository.clone(),
-        })
-        .serve(listener.incoming());
-
-    await!(server).expect("Server error");
-}
-
-async fn database_pool(database_url: String) -> Result<DbPool, tokio_postgres::Error> {
-    let postgres_connection =
-        bb8_postgres::PostgresConnectionManager::new(database_url, tokio_postgres::NoTls);
-
-    await!(bb8::Pool::builder().build(postgres_connection).compat())
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Config {
-    pub channel_list_limit: u32,
-}
-
-impl TryFrom<Vars> for Config {
-    type Error = domain::DomainError;
-
-    fn try_from(mut vars: Vars) -> Result<Self, Self::Error> {
-        let limit = vars
-            .find_map(|(key, value)| {
-                if key == "SENTRY_CHANNEL_LIST_LIMIT" {
-                    Some(value)
-                } else {
-                    None
-                }
-            })
-            .ok_or(DomainError::InvalidArgument(
-                "SENTRY_CHANNEL_LIST_LIMIT evn. variable was not passed".to_string(),
-            ))
-            .and_then(|value| {
-                value.parse::<u32>().map_err(|_| {
-                    DomainError::InvalidArgument(
-                        "SENTRY_CHANNEL_LIST_LIMIT is not a u32 value".to_string(),
-                    )
-                })
-            });
-
-        Ok(Self {
-            channel_list_limit: limit?,
-        })
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
     }
+}
+
+fn handle_routing(req: Request<Body>) -> Response<Body> {
+//fn handle_routing(req: Request<Body>, _config: Arc<Config>, _adapter: Arc<AdapterTypes>) -> Response<Body> {
+    if req.uri().path().starts_with("/channel") {
+        return Response::new(Body::from("Channel!!"));
+    }
+
+    not_found()
+}
+
+fn not_found() -> Response<Body> {
+    let mut response = Response::new(Body::from("Not found"));
+    let status = response.status_mut();
+    *status = StatusCode::NOT_FOUND;
+    response
 }
