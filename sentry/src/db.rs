4f6a5b23a8f494d0cbfa05f847072fdd6ae6ea24
Lachezar Lechev <lachezar@adex.network>
2019-10-25 04:32:01 +0300

    sentry - function for creating `redis` async connection

--- sentry/src/db.rs
+++ sentry/src/db.rs
@@ -1,6 +1,14 @@
+use futures::compat::Future01CompatExt;
 use lazy_static::lazy_static;
+use redis::aio::Connection;
+use redis::RedisError;
 
 lazy_static! {
     static ref REDIS_URL: String =
         std::env::var("REDIS_URL").unwrap_or_else(|_| String::from("redis://127.0.0.1:6379"));
+}
+
+pub async fn redis_connection() -> Result<Connection, RedisError> {
+    let client = redis::Client::open(REDIS_URL.as_str()).unwrap();
+    client.get_async_connection().compat().await
 }
