ba257faf6c8866f2b83a85efae2ad662b9f92678
Lachezar Lechev <lachezar@adex.network>
2019-10-25 03:05:33 +0300

    sentry - register `db` module and add `REDIS_URL` to it

--- sentry/src/db.rs
+++ sentry/src/db.rs
@@ -1,20 +1,6 @@
+use lazy_static::lazy_static;
 
-pub struct Storage {
-    pub channel: None,
-    pub event_aggregates: None,
-    pub validator_messages: None
-}
-
-impl Storage {
-    pub fn new(db_pool: DbPool) -> Self {
-        Self { db_pool }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+lazy_static! {
+    static ref REDIS_URL: String =
+        std::env::var("REDIS_URL").unwrap_or_else(|_| String::from("redis://127.0.0.1:6379"));
 }
