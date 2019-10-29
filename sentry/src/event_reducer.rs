78482f058151f0f48a90d56ed7892882c87d98db
Lachezar Lechev <lachezar@adex.network>
2019-10-29 09:35:12 +0200

    sentry - even_reduce - test `fn reduce()` - fix some logic - change API of the fn - primitives - sentry - `derive(Default)` for `AggregateEvents`

--- sentry/src/event_reducer.rs
+++ sentry/src/event_reducer.rs
@@ -3,17 +3,10 @@ use primitives::Channel;
 
 // @TODO: Remove attribute once we use this function!
 #[allow(dead_code)]
-pub(crate) fn reduce(
-    channel: &Channel,
-    mut initial_aggr: EventAggregate,
-    ev: &Event,
-) -> EventAggregate {
+pub(crate) fn reduce(channel: &Channel, initial_aggr: &mut EventAggregate, ev: &Event) {
     match ev {
         Event::Impression { publisher, .. } => {
-            let impression = initial_aggr
-                .events
-                .get("IMPRESSION")
-                .expect("Event IMPRESSION should exist in the EventAggregate");
+            let impression = initial_aggr.events.get("IMPRESSION");
 
             let merge = merge_impression_ev(impression, &publisher, &channel);
 
@@ -31,16 +24,16 @@ pub(crate) fn reduce(
         }
         _ => {}
     };
-
-    initial_aggr
 }
 
 fn merge_impression_ev(
-    impression: &AggregateEvents,
+    impression: Option<&AggregateEvents>,
     earner: &str,
     channel: &Channel,
 ) -> AggregateEvents {
-    let mut impression = impression.clone();
+    let mut impression = impression
+        .map(Clone::clone)
+        .unwrap_or_else(Default::default);
 
     let event_counts = impression
         .event_counts
@@ -55,4 +48,54 @@ fn merge_impression_ev(
     *event_payouts += &channel.spec.min_per_impression;
 
     impression
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use chrono::Utc;
+    use primitives::util::tests::prep_db::DUMMY_CHANNEL;
+    use primitives::BigNum;
+
+    #[test]
+    fn test_reduce() {
+        let mut channel: Channel = DUMMY_CHANNEL.clone();
+        channel.deposit_amount = 100.into();
+        // make immutable again
+        let channel = channel;
+
+        let mut event_aggr = EventAggregate {
+            channel_id: channel.id.clone(),
+            created: Utc::now(),
+            events: Default::default(),
+        };
+
+        let event = Event::Impression {
+            publisher: "myAwesomePublisher".to_string(),
+            ad_unit: None,
+        };
+
+        for _ in 0..101 {
+            reduce(&channel, &mut event_aggr, &event);
+        }
+
+        assert_eq!(event_aggr.channel_id, channel.id);
+
+        let impression_event = event_aggr
+            .events
+            .get("IMPRESSION")
+            .expect("Should have an Impression event");
+
+        let event_counts = impression_event
+            .event_counts
+            .get("myAwesomePublisher")
+            .expect("There should be myAwesomePublisher event_counts key");
+        assert_eq!(event_counts, &BigNum::from(101));
+
+        let event_payouts = impression_event
+            .event_counts
+            .get("myAwesomePublisher")
+            .expect("There should be myAwesomePublisher event_payouts key");
+        assert_eq!(event_payouts, &BigNum::from(101));
+    }
 }
