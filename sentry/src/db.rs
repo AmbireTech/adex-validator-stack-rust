663e302b3a62bd97527dc1c14555f72bf6398d6b
Lachezar Lechev <lachezar@adex.network>
2019-11-13 10:39:20 +0200

    sentry - db - DbPool type

--- sentry/src/db.rs
+++ sentry/src/db.rs
@@ -1,12 +1,12 @@
 use redis::aio::MultiplexedConnection;
 use redis::RedisError;
 
-use bb8::Pool;
-use bb8_postgres::{
-    tokio_postgres::{Error as PostgresError, NoTls},
-    PostgresConnectionManager,
-};
 use lazy_static::lazy_static;
+use bb8_postgres::PostgresConnectionManager;
+use bb8_postgres::tokio_postgres::NoTls;
+use bb8::Pool;
+
+pub type DbPool = Pool<PostgresConnectionManager<NoTls>>;
 
 lazy_static! {
     static ref REDIS_URL: String =
@@ -20,7 +20,7 @@ pub async fn redis_connection() -> Result<MultiplexedConnection, RedisError> {
     client.get_multiplexed_tokio_connection().await
 }
 
-pub async fn postgres_connection() -> Result<Pool<PostgresConnectionManager<NoTls>>, PostgresError>
+pub async fn postgres_connection() -> Result<DbPool, bb8_postgres::tokio_postgres::Error>
 {
     let pg_mgr = PostgresConnectionManager::new_from_stringlike(POSTGRES_URL.as_str(), NoTls)?;
 
