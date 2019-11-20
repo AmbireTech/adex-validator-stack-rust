e7c7ae13066074fcfc324bf5374d09f6b4e8896b
Omidiora Samuel <8148384+samparsky@users.noreply.github.com>
2019-11-20 10:41:34 +0100

    fix: add midleware example

--- sentry/src/lib.rs
+++ sentry/src/lib.rs
@@ -106,20 +106,10 @@ impl<A: Adapter + 'static> Application<A> {
                 return map_response_error(ResponseError::BadRequest("invalid auth".into()));
             }
         };
-
+        
         let mut response = match (req.uri().path(), req.method()) {
             ("/cfg", &Method::GET) => config(req, &self).await,
-            ("/channel", &Method::POST) => {
-                // example with middleware
-                // @TODO remove later
-                let req = match chain(req, vec![config_middleware]).await {
-                    Ok(req) => req,
-                    Err(error) => {
-                        return map_response_error(error);
-                    }
-                };
-                create_channel(req, &self).await
-            }
+            ("/channel", &Method::POST) => create_channel(req, &self).await,
             ("/channel/list", &Method::GET) => Err(ResponseError::NotFound),
             // This is important becuase it prevents us from doing
             // expensive regex matching for routes without /channel
@@ -134,6 +124,15 @@ impl<A: Adapter + 'static> Application<A> {
                         .get(1)
                         .map_or("".to_string(), |m| m.as_str().to_string())]);
                     req.extensions_mut().insert(param);
+
+                    // example with middleware
+                    // @TODO remove later
+                    let req = match chain(req, vec![config_middleware]).await {
+                        Ok(req) => req,
+                        Err(error) => {
+                            return map_response_error(error);
+                        }
+                    };
 
                     last_approved(req, &self).await
                 } else {
