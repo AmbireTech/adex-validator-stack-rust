e7b8eda3ebecaaf86aed4a3abe1888e3c0eb4daa
Lachezar Lechev <lachezar@adex.network>
2020-04-30 12:09:49 +0300

    primitives - AdSlot

---
+++ primitives/src/ad_slot.rs
@@ -0,0 +1,65 @@
+use crate::{BigNum, TargetingTag, ValidatorId};
+use chrono::serde::{ts_milliseconds, ts_milliseconds_option};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+/// See [AdEx Protocol adSlot.md][protocol] & [adex-models AdSlot.js][adex-models] for more details.
+/// [protocol]: https://github.com/AdExNetwork/adex-protocol/blob/master/adSlot.md
+/// [adex-models]: https://github.com/AdExNetwork/adex-models/blob/master/src/models/AdSlot.js
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct AdSlot {
+    /// valid ipfs hash of spec props below
+    pub ipfs: String,
+    /// The type of the AdSlot
+    /// currently, possible values are:
+    /// > legacy_300x250, legacy_250x250, legacy_240x400, legacy_336x280,
+    /// > legacy_180x150, legacy_300x100, legacy_720x300, legacy_468x60,
+    /// > legacy_234x60, legacy_88x31, legacy_120x90, legacy_120x60,
+    /// > legacy_120x240, legacy_125x125, legacy_728x90, legacy_160x600,
+    /// > legacy_120x600, legacy_300x600
+    /// see IAB ad unit guidelines and iab_flex_{adUnitName} (see IAB's new ad portfolio and PDF)
+    #[serde(rename = "type")]
+    pub ad_type: String,
+    /// A URL to the resource (usually PNG)
+    /// * must use the ipfs:// protocol, to guarantee data immutability
+    pub media_url: String,
+    /// MIME type of the media.
+    // Possible values at the moment are:
+    /// * image/jpeg
+    /// * image/png
+    pub media_mime: String,
+    /// Advertised URL
+    pub target_url: String,
+    /// Array of TargetingTag
+    pub targeting: Vec<TargetingTag>,
+    // HashMap<DepositAsset, BigNum> for the minimum payment accepted per impression
+    #[serde(default)]
+    pub min_per_impression: Option<HashMap<String, BigNum>>,
+    /// Array of TargetingTag
+    /// meant for discovery between publishers/advertisers
+    #[serde(default)]
+    pub tags: Vec<TargetingTag>,
+    #[serde(default)]
+    pub auto_tags: Vec<TargetingTag>,
+    /// Valid ipfs hash for Ad Unit object. It will be used as fallback data (optional)
+    pub fallback_unit: Option<String>,
+    /// User address from the session
+    pub owner: ValidatorId,
+    /// UTC timestamp in milliseconds, used as nonce for escaping duplicated spec ipfs hashes
+    #[serde(with = "ts_milliseconds")]
+    pub created: DateTime<Utc>,
+    /// the name of the unit used in platform UI
+    pub title: Option<String>,
+    /// arbitrary text used in platform UI
+    pub description: Option<String>,
+    #[serde(default)]
+    pub webiste: Option<String>,
+    /// user can change it - used for filtering in platform UI
+    #[serde(default)]
+    pub archived: bool,
+    /// UTC timestamp in milliseconds, changed every time modifiable property is changed
+    #[serde(default, with = "ts_milliseconds_option")]
+    pub modified: Option<DateTime<Utc>>,
+}
