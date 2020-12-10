508b3391da729f6d647391acf45ea25737e09928
samparsky <8148384+samparsky@users.noreply.github.com>
2020-12-10 08:46:46 +0200

    add: exhausted property to validator messages

--- validator_worker/src/leader.rs
+++ validator_worker/src/leader.rs
@@ -4,6 +4,7 @@ use primitives::adapter::{Adapter, AdapterErrorKind};
 use primitives::{
     validator::{Accounting, MessageTypes, NewState},
     BalancesMap,
+    BigNum,
 };
 
 use crate::heartbeat::{heartbeat, HeartbeatStatus};
@@ -48,11 +49,14 @@ async fn on_new_accounting<A: Adapter + 'static>(
 
     let signature = iface.adapter.sign(&state_root)?;
 
+    let exhausted = new_accounting.balances.clone().values().sum::<BigNum>() == iface.channel.deposit_amount;
+
     let propagation_results = iface
         .propagate(&[&MessageTypes::NewState(NewState {
             state_root,
             signature,
             balances: new_accounting.balances.clone(),
+            exhausted
         })])
         .await;
 
