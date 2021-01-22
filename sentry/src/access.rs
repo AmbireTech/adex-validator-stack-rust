af6538c8427a521f65d079be9a78ca4dbaf60ee8
simzzz <simeon@stremio.com>
2021-01-22 16:14:14 +0200

    Putting passing tests again

--- sentry/src/access.rs
+++ sentry/src/access.rs
@@ -221,11 +221,15 @@ mod test {
     use crate::Session;
 
     use super::*;
+    use serial_test::serial;
 
-    async fn setup() -> (Config, MultiplexedConnection) {
+    async fn setup(db_index: usize) -> (Config, MultiplexedConnection) {
         let mut redis = redis_connection().await.expect("Couldn't connect to Redis");
         let config = configuration("development", None).expect("Failed to get dev configuration");
-
+        let _ = redis::cmd("SELECT")
+            .arg(db_index)
+            .query_async::<_, String>(&mut redis)
+            .await;
         // run `FLUSHALL` to clean any leftovers of other tests
         let _ = redis::cmd("FLUSHALL")
             .query_async::<_, String>(&mut redis)
@@ -268,8 +272,9 @@ mod test {
     }
 
     #[tokio::test]
+    #[serial]
     async fn session_uid_rate_limit() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(0).await;
 
         let auth = Auth {
             era: 0,
@@ -322,8 +327,9 @@ mod test {
     }
 
     #[tokio::test]
+    #[serial]
     async fn ip_rate_limit() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(1).await;
 
         let auth = Auth {
             era: 0,
@@ -376,9 +382,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn check_access_past_channel_valid_until() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(2).await;
 
         let auth = Auth {
             era: 0,
@@ -416,9 +422,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn check_access_close_event_in_withdraw_period() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(3).await;
 
         let auth = Auth {
             era: 0,
@@ -456,9 +462,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn check_access_close_event_and_is_creator() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(4).await;
 
         let auth = Auth {
             era: 0,
@@ -496,9 +502,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn check_access_update_targeting_event_and_is_creator() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(5).await;
 
         let auth = Auth {
             era: 0,
@@ -536,9 +542,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn not_creator_and_there_are_close_events() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(6).await;
 
         let auth = Auth {
             era: 0,
@@ -587,9 +593,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn not_creator_and_there_are_update_targeting_events() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(7).await;
 
         let auth = Auth {
             era: 0,
@@ -637,9 +643,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn in_withdraw_period_no_close_events() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(8).await;
 
         let auth = Auth {
             era: 0,
@@ -677,9 +683,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn with_forbidden_country() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(9).await;
 
         let auth = Auth {
             era: 0,
@@ -716,9 +722,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn with_forbidden_referrer() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(10).await;
 
         let auth = Auth {
             era: 0,
@@ -755,9 +761,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn no_rate_limit() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(11).await;
 
         let auth = Auth {
             era: 0,
@@ -791,9 +797,9 @@ mod test {
     }
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn applied_rules() {
-        let (config, redis) = setup().await;
+        let (config, redis) = setup(12).await;
 
         let auth = Auth {
             era: 0,
