9ae51cc0e31f65ab5761cdd526ef36ce98f32033
Lachezar Lechev <lachezar@adex.network>
2021-03-16 15:05:48 +0200

    primitives - Cargo - update deps and remove duplicates

--- primitives/src/eth_checksum.rs
+++ primitives/src/eth_checksum.rs
@@ -1,12 +1,16 @@
-use crypto::{digest::Digest, sha3::Sha3};
+use tiny_keccak::{Hasher, Keccak};
 
 pub fn checksum(address: &str) -> String {
     let address = address.trim_start_matches("0x").to_lowercase();
 
     let address_hash = {
-        let mut hasher = Sha3::keccak256();
-        hasher.input(address.as_bytes());
-        hasher.result_str()
+        let mut hasher = Keccak::v256();
+        let mut result: [u8; 32] = [0; 32];
+
+        hasher.update(address.as_bytes());
+        hasher.finalize(&mut result);
+
+        hex::encode(result)
     };
 
     address
@@ -25,4 +29,24 @@ pub fn checksum(address: &str) -> String {
 
             acc
         })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_checksums() {
+        let expected_checksum = "0xce07CbB7e054514D590a0262C93070D838bFBA2e";
+
+        let non_checksummed = expected_checksum.to_lowercase();
+
+        assert_eq!(expected_checksum, checksum(&non_checksummed));
+
+        let non_prefixed = non_checksummed
+            .strip_prefix("0x")
+            .expect("should have prefix");
+
+        assert_eq!(expected_checksum, checksum(&non_prefixed))
+    }
 }
