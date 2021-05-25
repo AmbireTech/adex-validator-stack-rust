4c196b192ff88943c133b6faedcd30b47c938313
Lachezar Lechev <lachezar@adex.network>
2021-05-25 16:16:13 +0300

    sentry - accounting - Aggregator more impls and remove Postgres impl

--- sentry/src/accounting.rs
+++ sentry/src/accounting.rs
@@ -3,7 +3,8 @@ use std::{collections::HashMap, sync::Arc};
 use dashmap::{mapref::entry::Entry, DashMap};
 use futures::{stream::select, StreamExt};
 use primitives::{
-    sentry::accounting::{Balances, CheckedState},
+    channel_v5::Channel,
+    sentry::accounting::{Accounting, Balances, CheckedState},
     Address, ChannelId, UnifiedNum,
 };
 use thiserror::Error;
@@ -13,7 +14,7 @@ use tokio::{
 };
 use tokio_stream::wrappers::{IntervalStream, ReceiverStream};
 
-use self::store::{Client, Store};
+pub use self::store::{Client, Store};
 
 #[derive(Debug)]
 pub struct Payment {
@@ -25,7 +26,7 @@ pub type Spender = Address;
 pub type Payouts = HashMap<Spender, Payment>;
 
 struct Value {
-    channel: ChannelId,
+    channel: Channel,
     payouts: Payouts,
 }
 
@@ -41,7 +42,7 @@ pub enum Error {
     #[error("{0}")]
     Overflow(String),
     #[error(transparent)]
-    Sending(#[from] SendError<(ChannelId, Payouts)>),
+    Sending(#[from] SendError<(Channel, Payouts)>),
     #[error("The sum of Earning and the sum of Spending should be equal")]
     PayoutsDiffer,
 }
@@ -66,7 +67,7 @@ where
     }
 
     /// Validates that the `sum(earners)` == `sum(spenders)` before sending the payouts for recording
-    pub async fn record(&self, channel: ChannelId, payouts: Payouts) -> Result<(), Error> {
+    pub async fn record(&self, channel: Channel, payouts: Payouts) -> Result<(), Error> {
         self.sender
             .send(Value { channel, payouts })
             .await
@@ -97,8 +98,7 @@ where
 
             let update_interval =
                 IntervalStream::new(interval(period)).map(|_e| TimeFor::AggregatesUpdate);
-            let receiver_stream =
-                ReceiverStream::new(receiver).map(|v| TimeFor::EventAggregation(v));
+            let receiver_stream = ReceiverStream::new(receiver).map(TimeFor::EventAggregation);
 
             let mut select_time = select(update_interval, receiver_stream);
 
@@ -108,7 +108,7 @@ where
                         todo!("call persistence Store and get Accounting")
                     }
                     TimeFor::EventAggregation(Value { channel, payouts }) => {
-                        match aggregates.entry(channel) {
+                        match aggregates.entry(channel.id()) {
                             // entry that already exists and is fetched from Store
                             Entry::Occupied(mut entry) => {
                                 let balances = entry.get_mut();
@@ -121,9 +121,9 @@ where
                                 }
                             }
                             Entry::Vacant(vacant) => {
-                                let accounting = match store.fetch(channel).await {
+                                let existing_accounting = match store.fetch(channel.id()).await {
                                     Ok(accounting) => accounting,
-                                    Err(error) => {
+                                    Err(_error) => {
                                         // TODO: Log error from store
 
                                         // send the value back to try and update the accounting again.
@@ -135,21 +135,40 @@ where
                                     }
                                 };
 
-                                // let mut balances = Balances::<CheckedState>::default();
-                                // for (spender, payout) in payouts {
-                                //     balances.spend(spender, payout.earner, payout.amount);
-                                // }
+                                let accounting_result = match existing_accounting {
+                                    Some(Accounting {
+                                        mut balances,
+                                        channel,
+                                        ..
+                                    }) => {
+                                        for (spender, payout) in payouts {
+                                            balances
+                                                .spend(spender, payout.earner, payout.amount)
+                                                .expect("Should not overflow");
+                                        }
 
-                                /* let accounting = Accounting {
-                                    channel_id: channel,
-                                    balances,
-                                    updated_at: None,
-                                    created_at: Utc::now(),
-                                }; */
+                                        store.update(&channel, balances).await
+                                    }
+                                    None => {
+                                        let mut balances = Balances::<CheckedState>::default();
+                                        for (spender, payout) in payouts {
+                                            balances
+                                                .spend(spender, payout.earner, payout.amount)
+                                                .expect("Should not overflow");
+                                        }
 
-                                // try to save in Store the first time we receive a new value
+                                        store.create(channel, balances).await
+                                    }
+                                };
 
-                                // vacant.insert()
+                                match accounting_result {
+                                    Ok(accounting) => {
+                                        vacant.insert(accounting.balances);
+                                    }
+                                    Err(_err) => {
+                                        // TODO: Log Store error
+                                    }
+                                }
                             }
                         }
                     }
@@ -161,7 +180,7 @@ where
     pub async fn handle() {}
 }
 
-mod store {
+pub mod store {
     use async_trait::async_trait;
     use std::{ops::Deref, sync::Arc};
 
@@ -212,216 +231,6 @@ mod store {
             channel: &Channel,
             new_balances: Balances<CheckedState>,
         ) -> Result<Accounting<CheckedState>, Self::Error>;
-    }
-
-    // TODO: Move to DB
-    pub mod postgres {
-        use super::*;
-
-        use async_trait::async_trait;
-        use chrono::Utc;
-        use primitives::{channel_v5::Channel, sentry::accounting::Balances};
-        use std::convert::TryFrom;
-        use thiserror::Error;
-        use tokio_postgres::types::Json;
-
-        #[derive(Debug, Error)]
-        pub enum Error {
-            #[error("Accounting Balances error: {0}")]
-            Balances(#[from] primitives::sentry::accounting::Error),
-            #[error("Fetching Accounting from postgres error: {0}")]
-            Postgres(#[from] tokio_postgres::Error),
-            #[error(
-                "Creating or updating a record did not return the expected number of modified rows"
-            )]
-            NotModified,
-        }
-
-        pub struct Postgres {
-            client: deadpool_postgres::Client,
-        }
-
-        impl Postgres {
-            pub fn new(client: deadpool_postgres::Client) -> Self {
-                Self { client }
-            }
-        }
-
-        #[async_trait]
-        impl Client for Postgres {
-            type Error = Error;
-
-            /// ```text
-            /// SELECT channel, earners, spenders, created, updated FROM accounting WHERE channel_id = $1
-            /// ```
-            async fn fetch(
-                &self,
-                channel: ChannelId,
-            ) -> Result<Option<Accounting<CheckedState>>, Self::Error> {
-                let statement = self.client.prepare("SELECT channel, earners, spenders, created, updated FROM accounting WHERE channel_id = $1").await?;
-
-                let accounting = self
-                    .client
-                    .query_opt(&statement, &[&channel])
-                    .await?
-                    .as_ref()
-                    .map(Accounting::<CheckedState>::try_from)
-                    .transpose()
-                    .map_err(Error::Balances)?;
-
-                Ok(accounting)
-            }
-
-            /// ```text
-            /// INSERT INTO accounting (channel_id, channel, earners, spenders, updated, created) VALUES ($1, $2, $3, $4, $5, $6)
-            /// ```
-            async fn create(
-                &self,
-                channel: Channel,
-                balances: Balances<CheckedState>,
-            ) -> Result<Accounting<CheckedState>, Self::Error> {
-                let statement = self.client.prepare("INSERT INTO accounting (channel_id, channel, earners, spenders, updated, created) VALUES ($1, $2, $3, $4, $5, $6)").await?;
-
-                let earners = Json(&balances.earners);
-                let spenders = Json(&balances.spenders);
-                let updated = None;
-                let created = Utc::now();
-
-                let modified_rows = self
-                    .client
-                    .execute(
-                        &statement,
-                        &[
-                            &channel.id(),
-                            &channel,
-                            &earners,
-                            &spenders,
-                            &updated,
-                            &created,
-                        ],
-                    )
-                    .await?;
-
-                // we expect only a single row to be modified with this query!
-                if modified_rows == 1 {
-                    Ok(Accounting {
-                        channel,
-                        balances,
-                        updated,
-                        created,
-                    })
-                } else {
-                    Err(Error::NotModified)
-                }
-            }
-
-            async fn update(
-                &self,
-                channel: &Channel,
-                new_balances: Balances<CheckedState>,
-            ) -> Result<Accounting<CheckedState>, Self::Error> {
-                let statement = self.client.prepare("UPDATE accounting SET earners = $1::jsonb, spenders = $2::jsonb, updated = $3 WHERE channel_id = $4 RETURNING channel_id, channel, earners, spenders, updated, created").await?;
-
-                let earners = Json(&new_balances.earners);
-                let spenders = Json(&new_balances.spenders);
-                let updated = Some(Utc::now());
-
-                // we are using the RETURNING statement and selecting all field to return the new Accounting
-                let row = self
-                    .client
-                    .query_one(&statement, &[&earners, &spenders, &updated, &channel.id()])
-                    .await?;
-
-                let new_accounting = Accounting::try_from(&row)?;
-
-                Ok(new_accounting)
-            }
-        }
-
-        #[cfg(test)]
-        mod test {
-            use primitives::{
-                sentry::accounting::Balances,
-                util::tests::prep_db::{ADDRESSES, DUMMY_CAMPAIGN},
-            };
-
-            use crate::db::tests_postgres::{setup_test_migrations, DATABASE_POOL};
-
-            use super::*;
-
-            #[tokio::test]
-            async fn store_create_insert_and_update() {
-                let test_pool = DATABASE_POOL.get().await.expect("Should get test pool");
-
-                let client = Postgres::new(test_pool.get().await.expect("Should get client"));
-
-                setup_test_migrations(test_pool.clone())
-                    .await
-                    .expect("Migrations should succeed");
-
-                let channel = DUMMY_CAMPAIGN.channel.clone();
-                // Accounting that does not exist yet
-                {
-                    let non_existing = client
-                        .fetch(channel.id())
-                        .await
-                        .expect("Query should execute");
-
-                    assert!(
-                        non_existing.is_none(),
-                        "Accounting is empty, we expect no returned accounting for this Channel"
-                    );
-                }
-
-                // Create a new Accounting
-                let new_accounting = {
-                    let mut balances = Balances::<CheckedState>::default();
-                    balances
-                        .spend(ADDRESSES["creator"], ADDRESSES["publisher"], 1_000.into())
-                        .expect("Should not overflow");
-
-                    let actual_acc = client
-                        .create(channel.clone(), balances.clone())
-                        .await
-                        .expect("Should insert Accounting");
-
-                    let expected_acc = Accounting {
-                        channel,
-                        balances,
-                        updated: None,
-                        // we have to use the same `created` time for the expected Accounting
-                        created: actual_acc.created.clone(),
-                    };
-
-                    assert_eq!(expected_acc, actual_acc);
-
-                    actual_acc
-                };
-
-                // Update Accounting
-                {
-                    let mut new_balances = new_accounting.balances.clone();
-                    new_balances
-                        .spend(ADDRESSES["creator"], ADDRESSES["leader"], 500.into())
-                        .expect("Should not overflow");
-
-                    let updated_accounting = client
-                        .update(&new_accounting.channel, new_balances.clone())
-                        .await
-                        .expect("Should update and return the updated Accounting");
-
-                    assert!(
-                        updated_accounting.updated.is_some(),
-                        "the Updated time should be present now"
-                    );
-
-                    assert_eq!(
-                        &new_balances, &updated_accounting.balances,
-                        "Should update the new balances accordingly"
-                    );
-                }
-            }
-        }
     }
 }
 
