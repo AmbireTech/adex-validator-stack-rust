ba06705d3eb3a59fbf143286222a0113015146b2
Lachezar Lechev <lachezar@adex.network>
2021-05-25 12:45:01 +0300

    primitives - Accounting update & postgres TryFrom<&Row>

--- primitives/src/sentry/accounting.rs
+++ primitives/src/sentry/accounting.rs
@@ -3,7 +3,7 @@ use std::{
     marker::PhantomData,
 };
 
-use crate::{balances_map::UnifiedMap, Address, ChannelId, UnifiedNum};
+use crate::{balances_map::UnifiedMap, Address, channel_v5::Channel, UnifiedNum};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Deserializer, Serialize};
 use thiserror::Error;
@@ -11,13 +11,14 @@ use thiserror::Error;
 #[derive(Serialize, Debug, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct Accounting<S: BalancesState> {
-    pub channel_id: ChannelId,
+    pub channel: Channel,
     #[serde(flatten)]
     pub balances: Balances<S>,
-    pub updated_at: DateTime<Utc>,
+    pub updated: Option<DateTime<Utc>>,
+    pub created: DateTime<Utc>,
 }
 
-#[derive(Serialize, Debug, Clone, PartialEq, Eq)]
+#[derive(Serialize, Debug, Clone, PartialEq, Eq, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct Balances<S> {
     pub earners: UnifiedMap,
@@ -28,7 +29,26 @@ pub struct Balances<S> {
 
 impl Balances<UncheckedState> {
     pub fn check(self) -> Result<Balances<CheckedState>, Error> {
-        Balances::<CheckedState>::try_from(self)
+        let earned = self
+            .earners
+            .values()
+            .sum::<Option<UnifiedNum>>()
+            .ok_or_else(|| Error::Overflow("earners overflow".to_string()))?;
+        let spent = self
+            .spenders
+            .values()
+            .sum::<Option<UnifiedNum>>()
+            .ok_or_else(|| Error::Overflow("spenders overflow".to_string()))?;
+
+        if earned != spent {
+            Err(Error::PayoutMismatch { spent, earned })
+        } else {
+            Ok(Balances {
+                earners: self.earners,
+                spenders: self.spenders,
+                state: PhantomData::<CheckedState>::default(),
+            })
+        }
     }
 }
 
@@ -72,11 +92,11 @@ pub enum Error {
 
 pub trait BalancesState {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct CheckedState;
 impl BalancesState for CheckedState {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct UncheckedState;
 impl BalancesState for UncheckedState {}
 
@@ -84,26 +104,7 @@ impl TryFrom<Balances<UncheckedState>> for Balances<CheckedState> {
     type Error = Error;
 
     fn try_from(value: Balances<UncheckedState>) -> Result<Self, Self::Error> {
-        let earned = value
-            .earners
-            .values()
-            .sum::<Option<UnifiedNum>>()
-            .ok_or_else(|| Error::Overflow("earners overflow".to_string()))?;
-        let spent = value
-            .spenders
-            .values()
-            .sum::<Option<UnifiedNum>>()
-            .ok_or_else(|| Error::Overflow("spenders overflow".to_string()))?;
-
-        if earned != spent {
-            Err(Error::PayoutMismatch { spent, earned })
-        } else {
-            Ok(Self {
-                earners: value.earners,
-                spenders: value.spenders,
-                state: PhantomData::default(),
-            })
-        }
+        value.check()
     }
 }
 
@@ -113,10 +114,11 @@ mod de {
 
     #[derive(Deserialize)]
     struct DeserializeAccounting {
-        pub channel_id: ChannelId,
+        pub channel: Channel,
         #[serde(flatten)]
         pub balances: DeserializeBalances,
-        pub updated_at: DateTime<Utc>,
+        pub created: DateTime<Utc>,
+        pub updated: Option<DateTime<Utc>>,
     }
 
     impl<'de> Deserialize<'de> for Accounting<UncheckedState> {
@@ -127,9 +129,10 @@ mod de {
             let de_acc = DeserializeAccounting::deserialize(deserializer)?;
 
             Ok(Self {
-                channel_id: de_acc.channel_id,
+                channel: de_acc.channel,
                 balances: Balances::<UncheckedState>::try_from(de_acc.balances).map_err(serde::de::Error::custom)?,
-                updated_at: de_acc.updated_at,
+                created: de_acc.created,
+                updated: de_acc.updated,
             })
         }
     }
@@ -142,9 +145,10 @@ mod de {
             let unchecked_acc = Accounting::<UncheckedState>::deserialize(deserializer)?;
 
             Ok(Self {
-                channel_id: unchecked_acc.channel_id,
+                channel: unchecked_acc.channel,
                 balances: unchecked_acc.balances.check().map_err(serde::de::Error::custom)?,
-                updated_at: unchecked_acc.updated_at,
+                created: unchecked_acc.created,
+                updated: unchecked_acc.updated,
             })
         }
     }
@@ -188,6 +192,32 @@ mod de {
                 spenders: value.spenders,
                 state: PhantomData::<UncheckedState>::default(),
             }
+        }
+    }
+}
+
+#[cfg(feature = "postgres")]
+mod postgres {
+    use super::*;
+    use postgres_types::Json;
+    use tokio_postgres::Row;
+
+    impl TryFrom<&Row> for Accounting<CheckedState> {
+        type Error = Error;
+        
+        fn try_from(row: &Row) -> Result<Self, Self::Error> {
+            let balances = Balances::<UncheckedState> {
+                earners: row.get::<_, Json<_>>("earners").0,
+                spenders: row.get::<_, Json<_>>("spenders").0,
+                state: PhantomData::default(),
+            }.check()?;
+
+            Ok(Self {
+                channel: row.get("channel"),
+                balances,
+                updated: row.get("updated"),
+                created: row.get("created"),
+            })
         }
     }
 }
