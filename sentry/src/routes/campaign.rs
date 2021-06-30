93800894d62f5ec8b5ae4c24b68bbd13d64e25e7
simzzz <simeon@stremio.com>
2021-06-30 12:03:15 +0300

    fixed failing test

--- sentry/src/routes/campaign.rs
+++ sentry/src/routes/campaign.rs
@@ -258,6 +258,79 @@ pub mod update_campaign {
     }
 }
 
+pub async fn insert_events<A: Adapter + 'static>(
+    req: Request<Body>,
+    app: &Application<A>,
+) -> Result<Response<Body>, ResponseError> {
+    let (req_head, req_body) = req.into_parts();
+
+    let auth = req_head.extensions.get::<Auth>();
+    let session = req_head
+        .extensions
+        .get::<Session>()
+        .expect("request should have session");
+
+    let campaign = req_head
+        .extensions
+        .get::<Campaign>()
+        .expect("request should have a Campaign loaded");
+
+    let body_bytes = hyper::body::to_bytes(req_body).await?;
+    let mut request_body = serde_json::from_slice::<HashMap<String, Vec<Event>>>(&body_bytes)?;
+
+    let events = request_body
+        .remove("events")
+        .ok_or_else(|| ResponseError::BadRequest("invalid request".to_string()))?;
+
+    let processed = process_events(app, auth, session, campaign, events).await?;
+
+    Ok(Response::builder()
+        .header("Content-type", "application/json")
+        .body(serde_json::to_string(&SuccessResponse { success: processed })?.into())
+        .unwrap())
+}
+
+async fn process_events<A: Adapter + 'static>(
+    app: &Application<A>,
+    auth: Option<&Auth>,
+    session: &Session,
+    campaign: &Campaign,
+    events: Vec<Event>,
+) -> Result<bool, ResponseError> {
+    if &Utc::now() > &campaign.active.to {
+        return Err(ResponseError::BadRequest("Campaign is expired".into()));
+    }
+
+    //
+    // TODO #381: AIP#61 Spender Aggregator should be called
+    //
+
+    // handle events - check access
+    // handle events - Update targeting rules
+    // calculate payout
+    // distribute fees
+    // handle spending - Spender Aggregate
+    // handle events - aggregate Events and put into analytics
+
+    check_access(
+        &app.redis,
+        session,
+        auth,
+        &app.config.ip_rate_limit,
+        &campaign,
+        &events,
+    )
+    .await
+    .map_err(|e| match e {
+        access::Error::ForbiddenReferrer => ResponseError::Forbidden(e.to_string()),
+        access::Error::RulesError(error) => ResponseError::TooManyRequests(error),
+        access::Error::UnAuthenticated => ResponseError::Unauthorized,
+        _ => ResponseError::BadRequest(e.to_string()),
+    })?;
+
+    Ok(true)
+}
+
 #[cfg(test)]
 mod test {
     use primitives::{
@@ -372,9 +445,10 @@ mod test {
         let remaining = redis::cmd("GET")
             .arg(&key)
             .query_async::<_, Option<String>>(&mut redis.connection)
-            .await;
+            .await
+            .expect("should return None");
 
-        assert_eq!(remaining.is_err(), true)
+        assert_eq!(remaining, None)
     }
 
     // test get_campaigns_remaining_sum
@@ -382,76 +456,4 @@ mod test {
     // test get_remaining_for_multiple_campaigns
 
     // test get_remaining_for_campaign_from_redis
-}
-pub async fn insert_events<A: Adapter + 'static>(
-    req: Request<Body>,
-    app: &Application<A>,
-) -> Result<Response<Body>, ResponseError> {
-    let (req_head, req_body) = req.into_parts();
-
-    let auth = req_head.extensions.get::<Auth>();
-    let session = req_head
-        .extensions
-        .get::<Session>()
-        .expect("request should have session");
-
-    let campaign = req_head
-        .extensions
-        .get::<Campaign>()
-        .expect("request should have a Campaign loaded");
-
-    let body_bytes = hyper::body::to_bytes(req_body).await?;
-    let mut request_body = serde_json::from_slice::<HashMap<String, Vec<Event>>>(&body_bytes)?;
-
-    let events = request_body
-        .remove("events")
-        .ok_or_else(|| ResponseError::BadRequest("invalid request".to_string()))?;
-
-    let processed = process_events(app, auth, session, campaign, events).await?;
-
-    Ok(Response::builder()
-        .header("Content-type", "application/json")
-        .body(serde_json::to_string(&SuccessResponse { success: processed })?.into())
-        .unwrap())
-}
-
-async fn process_events<A: Adapter + 'static>(
-    app: &Application<A>,
-    auth: Option<&Auth>,
-    session: &Session,
-    campaign: &Campaign,
-    events: Vec<Event>,
-) -> Result<bool, ResponseError> {
-    if &Utc::now() > &campaign.active.to {
-        return Err(ResponseError::BadRequest("Campaign is expired".into()));
-    }
-
-    //
-    // TODO #381: AIP#61 Spender Aggregator should be called
-    //
-
-    // handle events - check access
-    // handle events - Update targeting rules
-    // calculate payout
-    // distribute fees
-    // handle spending - Spender Aggregate
-    // handle events - aggregate Events and put into analytics
-
-    check_access(
-        &app.redis,
-        session,
-        auth,
-        &app.config.ip_rate_limit,
-        &campaign,
-        &events,
-    )
-    .await
-    .map_err(|e| match e {
-        access::Error::ForbiddenReferrer => ResponseError::Forbidden(e.to_string()),
-        access::Error::RulesError(error) => ResponseError::TooManyRequests(error),
-        access::Error::UnAuthenticated => ResponseError::Unauthorized,
-        _ => ResponseError::BadRequest(e.to_string()),
-    })?;
-
-    Ok(true)
 }
