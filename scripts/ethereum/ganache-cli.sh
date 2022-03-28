28a32598bded16a918109d3b09cab0e637c7c5ac
Lachezar Lechev <lachezar@ambire.com>
2022-03-28 18:09:42 +0300

    scripts - ethereum - Dockerfile - Copy snapshots folders scripts - ethereum - use snapshot folder based on CHAIN_ID

--- scripts/ethereum/ganache-cli.sh
+++ scripts/ethereum/ganache-cli.sh
@@ -37,11 +37,12 @@
 # gasLimit = 4503599627370495 (0xfffffffffffff)
 #
 
+# Used to specify the chainId & snapshot folder of the contracts
 CHAIN_ID=${CHAIN_ID:-1337}
 
 node /app/ganache-core.docker.cli.js --gasLimit 0xfffffffffffff \
   --chainId=${CHAIN_ID} \
-  --db="./snapshot" \
+  --db="./snapshot_contracts_${CHAIN_ID}" \
   --deterministic \
   --mnemonic="diary west sketch curious expose decade symptom height minor layer carry man" \
   --account="0xf224ebee7563c68de46b3f57db880e7709c102f56ecedeaa4bccd15dea702815,9000000000000000000000000000" \
