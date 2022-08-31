f48aa8a3d917094c506630f695e5aa01b0668c78
simzzz <simzzz@users.noreply.github.com>
2022-08-31 08:40:41 +0000

    deploy: 8041ec5cc4ef9350267735dd6fb3d5c38bc5b42a

--- sentry/routes/routers/sidebar-items.js
+++ sentry/routes/routers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["analytics_router","`/v5/analytics` router"],["campaigns_router","`/v5/campaign` router"],["channels_router","`/v5/channel` router"],["units_for_slot_router","`/v5/units-for-slot` router"]]};
+window.SIDEBAR_ITEMS = {"enum":[["LeafFor",""]],"fn":[["analytics_router","`/v5/analytics` router"],["campaigns_router","`/v5/campaign` router"],["channels_router","`/v5/channel` router"],["units_for_slot_router","`/v5/units-for-slot` router"]]};
