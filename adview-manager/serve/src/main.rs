986ec73ee7894487bae08ac050921f16db1bdf29
Lachezar Lechev <lachezar@ambire.com>
2022-10-19 18:06:20 +0300

    adview serve - add tracing subscriber

--- adview-manager/serve/src/main.rs
+++ adview-manager/serve/src/main.rs
@@ -2,7 +2,9 @@ use adview_serve::app::Application;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    let tracing_subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(tracing_subscriber)
+    .expect("setting tracing default failed");
 
     Application::new()?.run().await?;
 
